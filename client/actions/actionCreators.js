a47e56cd8e73da153f992cc4b5f58c0b85c323b6
Nick <nickzhang@192-168-1-105.tpgi.com.au>
2017-11-14 23:36:56 +1100

    second

--- client/actions/actionCreators.js
+++ client/actions/actionCreators.js
@@ -3,12 +3,14 @@ export function changeChatStatus(){
     type:'CHANGE_CHAT_STATUS',
   }
 }
+
 export function sendInquiry(inquiry){
   return{
     type:'SEND_INQUIRY',
     inquiry
   }
 }
+
 export function sendReply(reply){
   return{
     type:'SEND_REPLY',
